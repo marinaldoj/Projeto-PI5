31294dbe94ee64e75b5ee21ac2e69139e7917ce9
Marinaldo Junior <carvalho.junior@soterotech.com.br>
2021-04-19 19:14:51 -0300

    Implementados estilos globais

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
+import GlobalStyles from './styles/GlobalStyles'
+
 function App() {
   return (
     <>
+    <GlobalStyles />
     </>
   );
 }
---
+++ frontend/src/styles/GlobalStyles.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html, body, #root{
+        height: 100%;
+    }
+
+    #root{
+        background-color: #016795;
+    }
+
+    *, button, input{
+        border: 0;
+        outline: 0;
+        font-family: 'Roboto', sans-serif;
+    }
+
+    button{
+        cursor: pointer;
+    }
+`;
